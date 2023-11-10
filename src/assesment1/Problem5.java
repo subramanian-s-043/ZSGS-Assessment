2118d10b6be08c8c73b48e641a03e7a51a54d459
subramanian-s-043 <113493600+subramanian-s-043@users.noreply.github.com>
2023-11-10 19:16:59 +0530

    Assessment

---
+++ src/assesment1/Problem5.java
@@ -0,0 +1,29 @@
+package assesment1;
+import java.util.*;
+
+public class Problem5 {
+ public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number of Strings to be Entered:");
+	int n=sc.nextInt();
+	String[] inp=new String[n];
+	for(int i=0;i<n;i++)
+	{
+		System.out.println("Enter the String"+(i+1));
+		sc.nextLine();
+		inp[i]=sc.nextLine();
+	}
+	for(int i=0;i<n;i++)
+	{
+		char[] str=inp[i].toCharArray();
+		Arrays.sort(str);
+		String temp="";
+		for(int j=str.length - 1;j>=0;j--)
+		{
+			temp+=str[j];
+			
+		}
+		System.out.println("String"+(i+1)+":" + temp);
+	}
+}
+}
